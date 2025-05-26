4541b3792995f0825ebd62d12c666add9a55ca8f
Andre <130280275+And-rix@users.noreply.github.com>
2025-05-26 20:03:22 +0200

    Update vdsm-arc-toolkit.sh

--- vdsm-arc-toolkit.sh
+++ vdsm-arc-toolkit.sh
@@ -19,7 +19,7 @@ clear
 create_header "vDSM-Arc-Toolkit"
 
 # Menu
-echo -e "${CONSOLE}${G} -- Menu --${X}"
+echo -e "${CONSOLE}${C} -- Menu --${X}"
 echo -e "${C}1)${X} CREATE ${C}new vDSM.Arc${X}"
 echo -e "${C}2)${X} UPDATE ${C}existing vDSM.Arc${X}"
 echo -e "${C}3)${X} ADD ${C}disks to a VM${X}"

18d892c5f0291a81991d67bbba8dad95703b83ec
Andre <130280275+And-rix@users.noreply.github.com>
2025-06-24 14:25:16 +0200

    Update laptop-hibernation.sh

--- laptop-hibernation.sh
+++ laptop-hibernation.sh
@@ -20,9 +20,9 @@ create_header "Laptop-Hibernation"
 
 # Info
 echo -e "This script will add the required lines into"
-echo "-------------------------"
+line
 echo -e "${R}'/etc/systemd/logind.conf'${X}"
-echo "-------------------------"
+line
 echo -e "to use a laptop as an PVE host"
 echo ""
 continue_script

2166af1300f2a60be8e5f12e01e1fa4e6938f9bb
Andre <130280275+And-rix@users.noreply.github.com>
2025-07-30 13:24:27 +0200

    Redirect to new file

--- vdsm-arc-toolkit.sh
+++ vdsm-arc-toolkit.sh
@@ -5,48 +5,5 @@
 # GitHub: https://github.com/And-rix
 # License: /LICENSE
 
-export LANG=en_US.UTF-8
-
-# Import Misc
-source <(curl -s https://raw.githubusercontent.com/And-rix/pve-scripts/refs/heads/main/misc/colors.sh)
-source <(curl -s https://raw.githubusercontent.com/And-rix/pve-scripts/refs/heads/main/misc/emojis.sh)
-source <(curl -s https://raw.githubusercontent.com/And-rix/pve-scripts/refs/heads/main/misc/functions.sh)
-
-# Clearing screen
-clear
-
-# Post message
-create_header "vDSM-Arc-Toolkit"
-
-# Menu
-line
-echo -e "${CONSOLE}${C} -- Menu --${X}"
-line
-echo -e "${C}1)${X} CREATE ${C}new vDSM.Arc${X}"
-echo -e "${C}2)${X} UPDATE ${C}existing vDSM.Arc${X}"
-echo -e "${C}3)${X} ADD ${C}disks to a VM${X}"
-line
-echo -e "${R}x) EXIT${X}"
-echo -e ""
-read -n 1 option
-
-    case "$option" in
-        1) #CREATE new vDSM.Arc
-            bash -c "$(curl -fsSL https://raw.githubusercontent.com/And-rix/pve-scripts/refs/heads/main/vdsm-arc-install.sh)"
-			;;
-		2) #UPDATE existing vDSM.Arc
-            bash -c "$(curl -fsSL https://raw.githubusercontent.com/And-rix/pve-scripts/refs/heads/main/vdsm-arc-update.sh)"
-			;;
-		3) #ADD disks to existing VM
-            bash -c "$(curl -fsSL https://raw.githubusercontent.com/And-rix/pve-scripts/refs/heads/main/vm-disk-update.sh)"
-			;;
-        x) #EXIT
-            echo ""
-            echo -e "${NOTOK}${C}Exiting the script.${X}"
-            echo ""
-            exit 0
-			;;
-        *) # False selection
-            echo -e "${WARN}${R}Invalid input. Please choose '1' | '2' | '3' | 'x'.${X}"
-			;;
-    esac
+# Forward to new location
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/And-rix/pve-scripts/refs/heads/main/vdsm/vdsm-arc-toolkit.sh)"

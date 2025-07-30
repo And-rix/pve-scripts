2319afa42f2ff50e8a8c1bf2d6a987be784ea7f4
Andre <130280275+And-rix@users.noreply.github.com>
2025-07-30 12:47:06 +0200

    Create misc.sh

---
+++ misc/misc.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# MIT License
+# Copyright (c) 2025 And-rix
+# GitHub: https://github.com/And-rix
+# License: /LICENSE
+
+# -----------------------------
+# Colors
+# -----------------------------
+BK='\033[0;30m' # Black
+R='\033[0;31m'  # Red
+G='\033[0;32m'  # Green
+Y='\033[0;33m'  # Yellow
+B='\033[0;34m'  # Blue
+M='\033[0;35m'  # Magenta
+C='\033[0;36m'  # Cyan
+W='\033[0;37m'  # White
+
+# -----------------------------
+# BG_Colors
+# -----------------------------
+BG_R='\033[41m'
+BG_G='\033[42m'
+BG_Y='\033[43m'
+BG_B='\033[44m'
+BG_M='\033[45m'
+BG_C='\033[46m'
+BG_W='\033[47m'
+
+# -----------------------------
+# Reset Color
+# -----------------------------
+X='\033[0m'
+
+# -----------------------------
+# Emojis
+# -----------------------------
+TAB="  "
+INFO="${TAB}ℹ️${TAB}${X}"
+START="${TAB}▶️${TAB}${X}"
+OK="${TAB}✅${TAB}${X}"
+NOTOK="${TAB}❌${TAB}${X}"
+WARN="${TAB}⚠️${TAB}${X}"
+DISK="${TAB}💾${TAB}${X}"
+CONSOLE="${TAB}📟${TAB}${X}"
+ROBOT="${TAB}🤖${TAB}${X}"
+TOOL="${TAB}🛠️${TAB}${X}"

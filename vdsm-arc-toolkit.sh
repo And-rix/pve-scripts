2bb76f7d4cdb9c876d42515b6b85e6c3b88f8dd2
Andre <130280275+And-rix@users.noreply.github.com>
2025-08-06 08:41:05 +0200

    Delete vdsm-arc-toolkit.sh

--- vdsm-arc-toolkit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# MIT License
-# Copyright (c) 2025 And-rix
-# GitHub: https://github.com/And-rix
-# License: /LICENSE
-
-# Forward to new location
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/And-rix/pve-scripts/refs/heads/main/vdsm/vdsm-arc-toolkit.sh)"
